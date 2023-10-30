1eb91a6712bd34528e8dcc77ea296ba39474f74a
Ahmed <alqossaba@gmail.com>
2023-10-30 01:29:30 -0400

    plane and box

--- src/main.js
+++ src/main.js
@@ -1,68 +1,115 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
 
-function main() {
-    const canvas = document.querySelector('#c');
-    const renderer = new THREE.WebGL1Renderer({ antialias: true, canvas });
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
 
-    const fov = 75;
-    const aspect = 2;
-    const near = 0.1;
-    const far = 5;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-
-    camera.position.z = 2;
-
-    const scene = new THREE.Scene();
-
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-
-    const color = 0xffffff;
-    const intensity = 2;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
-    scene.add(light);
-
-    function makeInstance(geometry, color, x) {
-        const material = new THREE.MeshPhongMaterial({ color });
-
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
-
-        cube.position.x = x;
-
-        return cube;
+class World {
+    constructor() {
+        this._Initialize();
     }
 
-    const cubes = [
-        makeInstance(geometry, 0x44aa88, 0),
-        makeInstance(geometry, 0x8844aa, -2),
-        makeInstance(geometry, 0xaa8844, 2),
-    ];
+    _Initialize() {
+        const canvas = document.querySelector('#c');
 
-    scene.add(cubes);
+        this._threejs = new THREE.WebGLRenderer({ antialias:true, canvas });
+        this._threejs.shadowMap.enabled = true;
+        this._threejs.shadowMap.type = THREE.PCFsoftShadowMap;
+        this._threejs.setPixelRatio(window.devicePixelRatio);
+        this._threejs.setSize(window.innerWidth, window.innerHeight);
 
-    renderer.render(scene, camera);
+        document.body.appendChild(this._threejs.domElement);
 
-    function render(time) {
-        time *= 0.001; // convert time to seconds
+        window.addEventListener(
+            'resize',
+            () => {
+                this._onWindowResize();
+            },
+            false
+        );
 
-        cubes.forEach((cube, ndx) => {
-            const speed = 1 + ndx * 0.1;
-            const rot = time * speed;
-            cube.rotation.x = rot;
-            cube.rotation.y = rot;
+        const fov = 60;
+        const aspect = 1920 / 1080;
+        const near = 1.0;
+        const far = 1000.0;
+        this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+        this._camera.position.set(75, 20, 0);
+
+        this._scene = new THREE.Scene();
+
+        let light = new THREE.DirectionalLight(0xffffff);
+        light.position.set(100, 100, 100);
+        light.target.position.set(0, 0, 0);
+        light.castShadow = true;
+        light.shadow.bias = 0.01;
+        light.shadow.mapSize.width = 2048;
+        light.shadow.mapSize.height = 2048;
+        light.shadow.camera.near = 1.0;
+        light.shadow.camera.far = 500;
+        light.shadow.camera.left = 200;
+        light.shadow.camera.right = -200;
+        light.shadow.camera.top = 200;
+        light.shadow.camera.bottom = -200;
+        this._scene.add(light);
+
+        light = new THREE.AmbientLight(0x404040);
+        this._scene.add(light);
+
+        const controls = new OrbitControls(
+            this._camera,
+            this._threejs.domElement
+        );
+        controls.target.set(0, 20, 0);
+        controls.update();
+
+        const loader = new THREE.CubeTextureLoader();
+        const texture = loader.load([
+            './resources/posx.jpg',
+            './resources/negx.jpg',
+            './resources/posy.jpg',
+            './resources/negy.jpg',
+            './resources/posz.jpg',
+            './resources/negz.jpg',
+        ]);
+        this._scene.background = texture;
+
+        const plane = new THREE.Mesh(
+            new THREE.PlaneGeometry(100, 100, 1, 1),
+            new THREE.MeshStandardMaterial({
+                color: 0xffffff,
+            })
+        );
+        plane.castShadow = false;
+        plane.receiveShadow = true;
+        plane.rotation.x = -Math.PI / 2;
+        this._scene.add(plane);
+
+        const box = new THREE.Mesh(
+            new THREE.BoxGeometry(2, 2, 2),
+            new THREE.MeshStandardMaterial({
+                color: 0x808080
+            })
+        );
+        box.position.set(0, 1, 0);
+        box.castShadow = true;
+        box.receiveShadow = true;
+        this._scene.add(box);
+
+        this._RAF();
+    }
+
+    _onWindowResize() {
+        this._camera.aspect = window.innerWidth / window.innerHeight;
+        this._camera.updateProjectionMastrix();
+        this._threejs.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    _RAF() {
+        requestAnimationFrame(() => {
+            this._threejs.render(this._scene, this._camera);
+            this._RAF();
         });
-
-        renderer.render(scene, camera);
-
-        requestAnimationFrame(render);
     }
-    requestAnimationFrame(render);
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    return main();
+    _APP = new World();
 });
